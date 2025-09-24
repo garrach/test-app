b9c42017bc246e3f3aca05cfb8b0437a1a031ace
garrach <garrach76@gmail.com>
2025-09-24 20:59:40 +0100

    commit 02

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,11 @@
-const nextJest = require("next/jest");
-
-const createJestConfig = nextJest({ dir: "./" });
-
-const customJestConfig = {
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  testEnvironment: "jest-environment-jsdom",
-  // Add this if your test files are in custom folders
-  testMatch: ["**/?(*.)+(test|spec).[jt]s?(x)"],
-  // Optional: Add this to help Jest know how to handle modules like CSS/images if needed
+module.exports = {
+  testEnvironment: 'jest-environment-jsdom',  // browser-like environment
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',   // handle JSX with Babel
+  },
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/$1",
+    '\\.(css|scss|sass)$': 'identity-obj-proxy', // mock CSS imports
   },
-  collectCoverage: true,
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
-  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // run after env setup
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
 };
-
-module.exports = createJestConfig(customJestConfig);
