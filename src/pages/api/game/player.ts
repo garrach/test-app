85142ac7eb784bbf6d0b9e01c71789e1de01a56b
garrach <garrach76@gmail.com>
2025-09-25 10:30:38 +0100

    commit 06

--- src/pages/api/game/player.ts
+++ src/pages/api/game/player.ts
@@ -9,5 +9,9 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  res.status(200).json({ name: "John Doe" });
+    const play={
+        id:"0",
+        inventory:"inventory"
+        }
+  res.status(200).json(play);
 }
